73632d1ff65d6bb19f0c598c35e5da502b7debab
dallen97 <dustinallen188@gmail.com>
2025-10-19 21:49:11 -0400

    Added login, register, and logout api

--- wadt-backend/wadtapp/urls.py
+++ wadt-backend/wadtapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:container_id>/", views.workbench, name="workbench"),
+    path('auth/register/', views.register_user, name='register_user'),
+    path('auth/register/', views.login_user, name='login_user'),
+    path('auth/logout/', views.logout_user, name='logout_user'),
 ]
--- wadt-backend/wadtapp/views.py
+++ wadt-backend/wadtapp/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.http import require_http_methods
+import json
 
 from .models import Container
 
@@ -13,3 +18,57 @@ def workbench(request, container_id):
     container = get_object_or_404(Container, pk=container_id)
     context = {"container": container}
     return render(request, "wadtapp/workbench.html", context)
+
+@require_http_methods(["POST"])
+def register_user(request):
+    #create user account
+    try:
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+
+        if not all([username, password]):
+            return JsonResponse({'error': 'Username and Password Required'}, status=400)
+        
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'error': 'Username already taken'}, status=409)
+        
+        user = User.objects.create_user(username=username, password=password)
+
+        return JsonResonse({'status': 'Success', 'message': 'User created successfully', 'user_id': user.id}, status=201)
+
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid JSON.'}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+@require_http_methods(["Post"])
+def login_user(request):
+    #logs in a user
+    try:
+        data = json.loasd(request.body)
+        username = data.get('username')
+        password = data.get('password')
+
+        if not all([username, password]):
+            return JsonResponse({'error': 'Username and Password Required'}, status=400)
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            #creates session id, that's sent back to browser as a cookie
+            login(request, user)
+            return JsonResponse({'status': 'success', 'message': 'Login successful.', 'user_id': user.id, 'username': user.username})
+        else:
+            return JsonResponse({'error': 'Invalid credentials.'}, status=401)
+
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid JSON.'}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+@require_http_methods(["POST"])
+def logout_user(request):
+    #logs out user and removes session data for user
+    logout(request)
+    return JsonResponse({'status': 'Success', 'message': 'Logout successful.'})
